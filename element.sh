2c33de3a1309f057a295d5fd4842f36b4e1cd519
Dominykas Simanavičius <dominykas.simanavicius@proton.me>
2024-04-17 08:33:07 +0000

    test

--- element.sh
+++ element.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 PSQL="psql -X --username=freecodecamp --dbname=periodic_table --tuples-only -c"
 
-MAIN_PROGRAM() {
-  if [[ -z $1 ]]
-  then
+FIRST(){if [ $# -eq 0 ]; then
     echo "Please provide an element as an argument."
-  else
-    PRINT_ELEMENT $1
-  fi
-}
+    exit 0
+fi}
 
 PRINT_ELEMENT() {
   INPUT=$1
